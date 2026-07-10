9799129ab444a6b38f378a69d53a94cf2cbc2bc2
Derek <derex1987@gmail.com>
2026-07-10 05:37:50 -0500

    go types notes

--- test.go
+++ test.go
@@ -4,5 +4,18 @@ import "fmt"
 // fmt is the standard library package for formatted I/O.
 
 func main()  {
+	// no semicolons in golang
 	fmt.Println("Hello World!")
+
+	// keyword first, type comes last
+	var x int = 3
+	var defaultInt int       // 0
+    var defaultString string // ""
+    var defaultBool bool     // false
+
+	// shorthand variable declaration
+	urmom := "Go"
+
+	// Unused Variables are Compiler Errors
+	// No Implicit Type Conversion, must cast to use different types
 }
