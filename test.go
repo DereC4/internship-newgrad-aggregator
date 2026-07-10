1395aa0196b79fe32b9853d9df10ebcc35e5bf1a
Derek <derex1987@gmail.com>
2026-07-10 05:44:20 -0500

    testing out go error and functions

--- test.go
+++ test.go
@@ -1,21 +1,33 @@
 package main
 
 import "fmt"
+
 // fmt is the standard library package for formatted I/O.
 
-func main()  {
+func add(a, b int) float64 {
+	return float64(a + b)
+}
+
+func main() {
 	// no semicolons in golang
 	fmt.Println("Hello World!")
 
 	// keyword first, type comes last
 	var x int = 3
 	var defaultInt int       // 0
-    var defaultString string // ""
-    var defaultBool bool     // false
+	var defaultString string // ""
+	var defaultBool bool     // false
 
 	// shorthand variable declaration
 	urmom := "Go"
 
 	// Unused Variables are Compiler Errors
 	// No Implicit Type Conversion, must cast to use different types
+	fmt.Println(x)
+	fmt.Println(defaultInt)
+	fmt.Println(defaultString)
+	fmt.Println(defaultBool)
+	fmt.Println(urmom)
+	fmt.Println(add(30000, 39))
+
 }
